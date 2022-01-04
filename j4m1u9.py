0da9c1ca3e3e4ecde9308c277d8b3bbce2a36419
jamiul957 <73727571+jamiul957@users.noreply.github.com>
2022-01-04 11:15:34 +0600

    Update and rename README.md to j4m1u9.py

---
+++ j4m1u9.py
@@ -0,0 +1,58 @@
+import requests
+
+from requests.structures import CaseInsensitiveDict
+
+import os
+
+import sys
+
+import time
+
+os.system("pip install requests")
+
+os.system("clear")
+
+red="\033[0;31m"          # Red
+
+yellow="\033[0;33m"       # Yellow
+
+green="\033[0;32m"        # Green
+
+color_off="\033[0m"       # Text Reset
+
+bblack="\033[1;30m"       # Black
+
+bred="\033[1;31m"         # Red
+
+ured="\033[4;31m"         # Red
+
+on_green="\033[42m"       # Green
+
+blue="\033[0;34m"         # Blue
+
+lightblue = '\033[94m'
+
+red = '\033[91m'
+
+white = '\33[97m'
+
+yellow = '\33[93m'
+
+green = '\033[1;32m'
+
+cyan  = "\033[96m"
+
+end = '\033[0m'
+
+purple="\033[0;35m"
+
+logo=(green+""" \n\x1b[1;91m<<------------------------------------------------->>\n\x1b[1;93m\xe2\x9e\xa3 Author : JAMIUL ISLAM \n\x1b[1;93m\xe2\x9e\xa3 From : Rajshahi \n\x1b[1;93m\xe2\x9e\xa3 FB group : Team x4ck \n\x1b[1;93m\xe2\x9e\xa3 Help : sent sms: https://www.faceboom.com/jamiul.islam.tuhin.01 .\n\x1b[1;91m<<-------------------------------------------------->>\n
+
+
+
+
+
+
+
+""")
+
